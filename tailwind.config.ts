8e6ead836cde8e68fff6d3d89892095e2900d283
Vivekha Sekar <vivekhasekar@gmail.com>
2025-09-26 09:09:57 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type {Config} from 'tailwindcss';
+import {fontFamily} from 'tailwindcss/defaultTheme';
 
 export default {
   darkMode: ['class'],
@@ -10,6 +11,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
         body: ['"PT Sans"', 'sans-serif'],
         headline: ['"PT Sans"', 'sans-serif'],
         code: ['monospace'],
